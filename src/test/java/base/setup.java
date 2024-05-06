7a7d635883e12d8b5151aaf15e19ef1cba74812b
GevorgKhachatryann <gevorgkhachatryan285@gmail.com>
2024-05-06 12:23:30 +0400

    fixed

--- src/test/java/base/setup.java
+++ src/test/java/base/setup.java
@@ -3,6 +3,7 @@ package base;
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
@@ -16,10 +17,12 @@ public class setup {
 
     @BeforeMethod
     public static void beforeClass() {
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--headless"); // Enable headless mode
+        options.addArguments("--no-sandbox"); // Bypass OS security model
+        options.addArguments("--disable-dev-shm-usage"); // Overcome limited resource problems
         WebDriverManager.chromedriver().setup();
-        driver = new ChromeDriver();
-        wait = new WebDriverWait(driver, Duration.ofSeconds(25));
-        driver.manage().window().maximize();
+        driver = new ChromeDriver(options);
     }
 
     @AfterMethod
