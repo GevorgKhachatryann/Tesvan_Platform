393ef68122da13ba5f8b826b1ab47af06dd3267a
GevorgKhachatryann <gevorgkhachatryan285@gmail.com>
2024-05-15 15:12:55 +0400

    fixed

--- src/test/java/locators/SettingsLocators.java
+++ src/test/java/locators/SettingsLocators.java
@@ -3,6 +3,6 @@ package locators;
 import org.openqa.selenium.By;
 
 public class SettingsLocators {
-    public By settingsIcon = By.xpath("//*[@id=\"root\"]/div/div/div/ul/li[7]/a");
+    public By settingsIcon = By.cssSelector("ul > li:nth-child(7) > a");
 
 }
