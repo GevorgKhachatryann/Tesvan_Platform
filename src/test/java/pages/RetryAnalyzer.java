4998715510f620ef531d32e56341aef62016a0f1
GevorgKhachatryann <gevorgkhachatryan285@gmail.com>
2024-05-16 13:46:43 +0400

    retry

---
+++ src/test/java/pages/RetryAnalyzer.java
@@ -0,0 +1,19 @@
+package pages;
+
+import org.testng.IRetryAnalyzer;
+import org.testng.ITestResult;
+
+public class RetryAnalyzer implements IRetryAnalyzer {
+
+    private int retryCount = 0;
+    private static final int MAX_RETRY_COUNT = 5;
+
+    public boolean retry(ITestResult result) {
+        if (retryCount < MAX_RETRY_COUNT) {
+            retryCount++;
+            return true;
+        }
+        return false;
+    }
+
+}
